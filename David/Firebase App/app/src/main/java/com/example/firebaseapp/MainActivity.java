2a8ebdb7ebc9047ee981f06cc1b74a3d127fb8fa
davidshelby <dshelby@ntst.com>
2020-09-17 22:40:31 -0500

    Adding Firebase App

---
+++ David/Firebase App/app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.firebaseapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Currency;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            setContentView(R.layout.activity_main);
+        }
+        else {
+            Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+            MainActivity.this.startActivity(intent);
+        }
+    }
+
+}
