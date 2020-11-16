561c66014beb7f466cff97f3fd899c3a72cd684f
Mitchell Meier <mdmfvz@mst.edu>
2020-11-16 15:11:38 -0600

    line graph changes

--- JimSSGym/app/src/main/java/com/example/jimssgym/Capacity.java
+++ JimSSGym/app/src/main/java/com/example/jimssgym/Capacity.java
@@ -9,33 +9,51 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 public class Capacity extends AppCompatActivity{
 
     BarChart barChart;
+    ArrayList<String> dates;
+    Random random;
+    ArrayList<BarEntry> barEntries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capacity);
 
-        barChart = (BarChart) findViewById(R.id.bargraph);
+        LineChart lineChart = (LineChart) findViewById(R.id.chart);
 
-        ArrayList<BarEntry> barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(44f,0));
-        barEntries.add(new BarEntry(88f,1));
-        barEntries.add(new BarEntry(66f,2));
-        barEntries.add(new BarEntry(12f,3));
-        barEntries.add(new BarEntry(19f,4));
-        barEntries.add(new BarEntry(91f,5));
-        BarDataSet barDataSet = new BarDataSet(barEntries,"Dates");
+        //createRandomBarGraph("2016/05/05", "2016/06/01");
+
+
+
+        List<Entry> entries = new ArrayList<Entry>();
+        entries.add(new BarEntry(0,20));
+        entries.add(new BarEntry(1,15));
+        entries.add(new BarEntry(2,24));
+        entries.add(new BarEntry(3,2));
+        entries.add(new BarEntry(4,10));
+        entries.add(new BarEntry(5,28));
+        LineDataSet lineDataSet = new LineDataSet(entries,"Dates");
 
         ArrayList<String> theDates = new ArrayList<>();
         theDates.add("April");
@@ -45,14 +63,76 @@ public class Capacity extends AppCompatActivity{
         theDates.add("August");
         theDates.add("September");
 
-        BarData theData = new BarData(barDataSet);
-        barChart.setData(theData);
+        LineData theData = new LineData(lineDataSet);
+        lineChart.setData(theData);
+        lineChart.invalidate();
 
-        barChart.setTouchEnabled(true);
-        barChart.setDragEnabled(true);
-        barChart.setScaleEnabled(true);
+
+
+
 
     }
+    /*
+
+    public void createRandomBarGraph (String Date1, String Date2){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+
+        try{
+            Date date1 = simpleDateFormat.parse(Date1);
+            Date date2 = simpleDateFormat.parse(Date2);
+
+            Calendar mDate1 = Calendar.getInstance();
+            Calendar mDate2 = Calendar.getInstance();
+            mDate1.clear();
+            mDate2.clear();
+
+            mDate1.setTime(date1);
+            mDate1.setTime(date2);
+
+            dates = new ArrayList<>();
+            dates = getList(mDate1, mDate2);
+
+            barEntries = new ArrayList<>();
+            float max = 0f;
+            float value = 0f;
+            random = new Random();
+            for(int j=0; j<dates.size(); j++){
+                max = 100f;
+                value = random.nextFloat()*max;
+                barEntries.add(new BarEntry(value, j));
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        BarDataSet barDataSet = new BarDataSet(barEntries,"Dates");
+        BarData barData = new BarData(dates, barDataSet);
+        barChart.setData(barData);
+    }
+
+    public ArrayList<String> getList(Calendar startDate, Calendar endDate){
+        ArrayList<String> list = new ArrayList<>();
+        while(startDate.compareTo(endDate)<=0){
+            list.add(getDate(startDate));
+            startDate.add(Calendar.DAY_OF_MONTH,1);
+        }
+        return list;
+    }
+
+    public String getDate(Calendar cld){
+        String curDate = cld.get(Calendar.YEAR) + "/" + (cld.get(Calendar.MONTH) + 1) + "/"
+                + cld.get(Calendar.DAY_OF_MONTH);
+
+        try{
+            Date date = new SimpleDateFormat("yyyy/MM/dd").parse(curDate);
+            curDate = new SimpleDateFormat("yyy/MM/dd").format(date);
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
+        return curDate;
+    }
+    */
 
 
 }
